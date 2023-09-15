99501c6abc0fd5407797727b93866298e2c7a637
MILLER <aggregate.music@mac.com>
2023-09-15 15:33:30 +0000

    Init commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.write("Web service...coming right up!");
+    res.end();
+}).listen(3000);
+
+console.log("Server started on port 3000");
